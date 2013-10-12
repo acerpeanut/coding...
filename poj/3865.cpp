30519d9d37bcc55f82bb1bd3a75b79dec548db68
acerpeanut <peanut55562@gmail.com>
2013-10-12 19:39:16 +0800

    poj problem id 3865 version 2

--- poj/3865.cpp
+++ poj/3865.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#include <vector>
+// #include <vector>
 #include <cstring>
 using namespace std;
-class smp{
+/*class smp{
 public:
 	int a,b,c,d;
 	smp(int e,int f,int g,int h):a(e),b(f),c(g),d(h){}
-};
-
+};*/
 typedef char ch[100];
 int main(){
 	int a,b;
-	vector<smp> s;
+//	vector<smp> s;
 	cin>>a>>b;
 	ch **t=new ch*[a];
 	for(int i=0;i<a;i++)
@@ -23,7 +22,7 @@ int main(){
 		}
 		cin>>t[i][b-1];
 	}
-	for(int j=0;j<b;j++){
+/*	for(int j=0;j<b;j++){
 		for(int i=0;i<a-1;i++){
 			for(int k=i+1;k<a;k++){
 				if(strcmp(t[i][j],t[k][j])==0){
@@ -36,6 +35,22 @@ int main(){
 						}
 					}
 					s.push_back(smp(i,j,k,j));
+				}
+			}
+		}
+	}*/
+	for(int i=0;i<a-1;i++){
+		for(int k=i+1;k<a;k++){
+			int flag=-1;
+			for(int j=0;j<b;j++){
+				if(strcmp(t[i][j],t[k][j])==0){
+					if(flag==-1) flag=j;
+					else{
+						cout<<"NO"<<endl;
+						cout<<i+1<<" "<<k+1<<endl;
+						cout<<flag+1<<" "<<j+1<<endl;
+						return 0;
+					}
 				}
 			}
 		}
